bed098a4ba7dede893c97d9381d594b762cc04d6
Ben Ho <tounsoo@gmail.com>
2024-08-04 07:50:05 -0700

    remove rankbykeys

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,4 +1,3 @@
 export { getLabel } from "./getLabel";
 export { mergeRefs } from "./mergeRefs";
-export { rankFilterByKey, type WithRelevance } from "./rankFilterByKey";
 export { sortByKey } from "./sortByKey";
